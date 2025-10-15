8c285648d4a7506f6f6a7e81a2e0b3d4fd9791a4
Sara Salzano <sarasalzano95@gmail.com>
2025-10-15 14:24:17 +0200

    crea array post

--- index.js
+++ index.js
@@ -8,15 +8,28 @@ const port = 3000;
 //accedo ai file statici dalla cartella public
 app.use(express.static("public"));
 
+//crea array post
+const posts = [
+    {
+        "nome":"ciambellone",
+        "contenuto":"ricetta ciambellone",
+        "immagine":"/img/ciambellone.jpeg",
+        "tags": ["colazione", "torte", "dolci"],
+    },
+];
+
+
+app.get("/bacheca", (req, res)=>{
+    res.json(posts);
+});
+
+
 // definisce la route principale "/" e invia una risposta al browser
 app.get("/", (req, res)=>{
     res.send("<h1>Server del mio blog</h1>")
-})
-
-
-
+});
 
 //avvia il server sulla porta specificata e mostra un messaggio in console quando è pronto
 app.listen(port, ()=> {
     console.log(`example listening on port ${port}`)
-})
+});
