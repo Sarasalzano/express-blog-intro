974de0192ebb4734364eea120b1f6038cdf2a4fc
Sara Salzano <sarasalzano95@gmail.com>
2025-10-15 13:10:40 +0200

    import immagini

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const express = require("express")
+//importo framework express
+const express = require("express");
+//creo un'app
+const app = express();
+//imposto la porta su cui il server ascolta
+const port = 3000;
+
+app.use()
